c42012fd0ec543d6112ca82922d0c342a74d582f
Tugay <5942003+tugaykosekahya@users.noreply.github.com>
2023-08-26 20:27:03 +0300

    Update onlyDeploy.cmd

--- edocument-dev-server/onlyDeploy.cmd
+++ edocument-dev-server/onlyDeploy.cmd
@@ -1,5 +1,5 @@
-set GR_TMCT_HM=C:\Users\engin\projeler\apache-tomcat-7.0.93\
+set GR_TMCT_HM=C:\Users\apache-tomcat-7.0.93\
 c:
-cd C:\Users\engin\projeler\evdb-dev-server
+cd C:\Users\evdb-dev-server
 gradle clean undeploy deployExploded
 cd %GR_TMCT_HM%
